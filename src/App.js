e4dda1acf70d14329002856ca34ccf68dee905be
Mihael Stormrage <mihan.msn@hotmail.com>
2020-10-16 06:36:52 +0700

    Implement FetchData from SpaceX API class

--- src/App.js
+++ src/App.js
@@ -6,14 +6,25 @@ import Features from './components/Features';
 // import Calendar from './components/Calendar';
 // import Details from './components/Details';
 
+import FetchData from './service/FetchData';
+
 import './style.css';
 import Footer from "./components/Footer";
 
 class App extends React.Component {
 
+  fetchData = new FetchData();
+
   state = {
     rocket: 'Falcon 1',
   };
+
+  componentDidMount() {
+  }
+
+  updateRocket() {
+
+  }
 
   render() {
     return (
