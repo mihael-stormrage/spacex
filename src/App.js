3ec9b3327305eecfa1e1af2ecc5b12421db0dbd4
Mihael Stormrage <mihan.msn@hotmail.com>
2020-10-16 04:35:46 +0700

    Add Calendar component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
 import Features from './components/Features';
+// import Calendar from './components/Calendar';
 
 import './style.css';
 import Footer from "./components/Footer";
@@ -14,6 +15,7 @@ function App() {
       <Main />
       <Features />
       <Footer />
+      {/*<Calendar />*/}
     </>
   );
 }
