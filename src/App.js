cb352c2564e539b4a8eba5dba55c43630d5342af
Mihael Stormrage <mihan.msn@hotmail.com>
2020-10-16 04:18:34 +0700

    Add Features component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Header from './components/Header';
 import Main from './components/Main';
+import Features from './components/Features';
 
 import './style.css';
 
@@ -10,6 +11,7 @@ function App() {
     <>
       <Header />
       <Main />
+      <Features />
     </>
   );
 }
