f139ec4526f9e3bb5658caba8aeecfd5a807ff5d
Mihael Stormrage <mihan.msn@hotmail.com>
2020-10-16 07:46:27 +0700

    Render planet video

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './main.css';
 
+const video = {
+  'Falcon 1': 'moon',
+  'Falcon 9': 'earth',
+  'Falcon Heavy': 'mars',
+  other: 'space',
+};
+
 const Main = ({ rocket }) => (
   <section className="main">
     <h1 className="title">
@@ -8,8 +15,12 @@ const Main = ({ rocket }) => (
     </h1>
 
     <div className="video-container">
-      <video className="video" autoPlay loop muted src="./video/moon.mp4"></video>
-
+      <video className="video"
+             autoPlay loop muted
+             src={`./video/${video.hasOwnProperty(rocket) ? 
+               video[rocket] : 
+               video.other}.mp4`
+             } />
     </div>
   </section>
 );
