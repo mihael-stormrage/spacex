46575bd8facdecf0af1379519dc9ce001a5f4cbe
Mihael Stormrage <mihan.msn@hotmail.com>
2020-10-18 07:02:44 +0700

    Rework Header (Link & NavLink)

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,25 +1,26 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
 import logo from '../../logo.svg';
 import './header.css';
 
 const Header = (props) => (
   <header className="header">
-    <img
-      src={logo}
-      alt="Logo Space X"
-      className="logo"
-    />
+    <Link to='/'>
+      <img
+        src={logo}
+        alt="Logo Space X"
+        className="logo"
+      />
+    </Link>
     <nav className="main-nav nav">
       <ul className="list">
         {props.rockets.map((item, i) => (
           <li key={i} className="item">
-            <a href="/"
-               onClick={e => {
-                 e.preventDefault();
-                 props.changeRocket(item);
-               }}
+            <Link to="/rocket"
+               onClick={() => props.changeRocket(item)}
                className="item-link">{item}
-            </a>
+            </Link>
           </li>
         ))}
       </ul>
@@ -27,10 +28,14 @@ const Header = (props) => (
     <nav className="secondary-nav">
       <ul className="list">
         <li className="item">
-          <a href="/" className="item-link">Home</a>
+          <NavLink exact to="/"
+                   className="item-link"
+                   activeClassName="active">Home</NavLink>
         </li>
         <li className="item">
-          <a href="/calendar" className="item-link">Calendar</a>
+          <NavLink to="/calendar"
+                   className="item-link"
+                   activeClassName="active">Calendar</NavLink>
         </li>
       </ul>
     </nav>
